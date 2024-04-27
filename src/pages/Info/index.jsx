33a88d5ba904970a6f5881479a94b1ea09bd35ad
NABIL234234 <sarzanovnabil108@gmail.com>
2024-04-27 21:56:30 +0600

    Completed Orbitians page

--- src/pages/Info/index.jsx
+++ src/pages/Info/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Orbit from "../Info/Orbit/Orbit"
+import NftsOrbit from "./NftsOrbit/NftsOrbit"
 
 export default function index() {
   return (
 <>
-
+ <Orbit />
+ <NftsOrbit />
 </>
   )
 }
--- src/pages/Profile/Hero/Hero.jsx
+++ src/pages/Profile/Hero/Hero.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Hero.css";
 import GetStart from "../../../components/buttons/GetStart";
 
-
 // images
 import Anumakid from "../../../assets/IMAGE/SECTION/Anumakig_profile.png";
 import Copy from "../../../assets/IMAGE/PLAY.SVG/nav/Copy.svg";
@@ -13,20 +12,22 @@ import Youtube from "../../../assets/IMAGE/SECTION/Youtube.svg";
 import Twitter from "../../../assets/IMAGE/SECTION/Twitter.svg";
 import Instagram from "../../../assets/IMAGE/SECTION/instagram.svg";
 
-
 export default function Hero() {
   return (
     <>
       <div className="HeaderChannel" />
       <div className="max-w-6xl mx-auto px-5 font-mono">
-        <div className="absolute top-[393px] z-[1] ">
+        <div className="absolute top-[370px] lgg:top-[393px] z-[1] ">
           <img src={Anumakid} alt="Anumakid" />
         </div>
         <div className="pt-[80px]">
-          <div className="flex items-center">
+          <div className="block mdd:flex items-center">
             <h2 className=" text-white text-6xl font-semibold">Anumakid</h2>
             <div className="flex gap-[20px] ml-auto">
-              <GetStart imgUrl={Copy} desc={0xc0e3b79c} />
+              <div>
+                <GetStart imgUrl={Copy} desc={0xc0e3b79c} />
+              </div>
+
               <div className="flex w-full mb:w-48 items-center justify-center gap-3 rounded-xl text-white border-2 border-purple-500 p-2 mt-6  ">
                 <img src={Plus} alt="Rocket" />
                 <a href="#">Follow</a>
@@ -67,24 +68,23 @@ export default function Hero() {
         <div className="flex mt-[80px] ">
           <div className="flex justify-center items-center gap-[16px] w-[525px] pb-[14.5px]">
             <h3 className="text-white font-semibold">Created</h3>
-            <div className="bg-zinc-400 w-[47px] pt-0.5 pb-0.5 px-2.5 rounded-full">
+            <div className="hidden mb:flex bg-zinc-400 w-[47px] pt-0.5 pb-0.5 px-2.5 rounded-full">
               <h5 className="text-white">300</h5>
             </div>
           </div>
           <div className="flex justify-center items-center  gap-[16px] w-[525px] pb-[14.5px]">
             <h3 className="text-stone-400 font-semibold">Owned</h3>
-            <div className="bg-zinc-400 w-[37px] pt-0.5 pb-0.5 px-2.5 rounded-full">
+            <div className="hidden mb:flex bg-zinc-400 w-[37px] pt-0.5 pb-0.5 px-2.5 rounded-full">
               <h5 className="text-white">70</h5>
             </div>
           </div>
           <div className="flex justify-center items-center  gap-[16px] w-[525px] pb-[14.5px]">
             <h3 className="text-stone-400 font-semibold">Collection</h3>
-            <div className="bg-zinc-400 w-[37px] pt-0.5 pb-0.5 px-2.5 rounded-full">
+            <div className="hidden mb:flex bg-zinc-400 w-[37px] pt-0.5 pb-0.5 px-2.5 rounded-full">
               <h5 className="text-white">70</h5>
             </div>
           </div>
         </div>
-        
       </div>
     </>
   );
