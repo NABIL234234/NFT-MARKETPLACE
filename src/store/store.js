d19a744f19417d4292da8f6ff957ad85f7d6c63a
NABIL234234 <sarzanovnabil108@gmail.com>
2024-05-17 17:13:11 +0600

    Added new confirmCode Slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./slices/registerSlice"
+import confirnCode from "./slices/confirnCode";
 
 const store = configureStore({
   reducer: {
-   users: userSlice
+   users: userSlice,
+   confirmCode: confirnCode,
   }
 })
 
