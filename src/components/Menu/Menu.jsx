921729ac9cf17f721517e20377019a5c76476294
NABIL234234 <sarzanovnabil108@gmail.com>
2024-05-30 20:04:18 +0600

    fix: Fixed burger menu(close btn)

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Menu.scss";
 
+
 // images
+import market from "../../../src/assets/IMAGE/PLAY.SVG/nav/Storefront.svg";
+
 
 export default function Menu() {
   return (
     <div className="menu">
       <div className="menu_content">
         <ul>
+          <li>
+          <img className="absolute left-9 top-[9.5%]"  src={market} alt="market" />
+            <NavLink to="/" className=" text-xl smm:text-2xl">
+              NFT Marketplace
+            </NavLink>
+          </li>
           <li>
             <NavLink to="/shop" className="text-white">
               Marketplace
