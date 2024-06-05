e465e6f54ad49e45b2651bb4b9c8b67f2cc25c95
NABIL234234 <sarzanovnabil108@gmail.com>
2024-06-05 13:48:15 +0600

    feat: New Live create Nft

--- src/components/CardMoreNft/CardMoreNft.jsx
+++ src/components/CardMoreNft/CardMoreNft.jsx
@@ -2,22 +2,19 @@ import React from "react";
 import "./CardMoreNft.css";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { AiFillFire } from "react-icons/ai";
 
-export default function CardMoreNft({
-  imgUrl,
-  title,
-  avatar,
-  user,
-  price,
-  Bid,
-}) {
+export default function CardMoreNft({ imgUrl, title, avatar, user, price }) {
   return (
     <>
-      <motion.div  whileHover={{ scale: 1.1 }}
-  onHoverStart={e => {}}
-  onHoverEnd={e => {}} className="Card">
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        onHoverStart={(e) => {}}
+        onHoverEnd={(e) => {}}
+        className="Card"
+      >
         <Link to="/info">
-          <div className="bg-zinc-800 rounded-3xl">
+          <div className="bg-zinc-800 rounded-3xl w-full">
             <div className="w-full">
               <img className="w-full object-cover" src={imgUrl} alt="Galaxy" />
             </div>
@@ -31,12 +28,11 @@ export default function CardMoreNft({
               </div>
               <div className="flex pt-[25px]">
                 <div>
-                  <h5 className="text-stone-400">Price</h5>
+                  <h5 className="text-stone-400">Price $</h5>
                   <h5 className="">{price}</h5>
                 </div>
-                <div className="ml-auto">
-                  <h5 className="text-stone-400">Highest Bid</h5>
-                  <h5 className="">{Bid}</h5>
+                <div className="ml-auto pt-6">
+                  <AiFillFire />
                 </div>
               </div>
             </div>
--- src/pages/CreateNft/CreateNft.jsx
+++ src/pages/CreateNft/CreateNft.jsx
@@ -1,13 +1,11 @@
-// components/CreateNftComponent.jsx
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { createNft } from "../../store/slices/nft";
-
-// images
 import { RiNftFill } from "react-icons/ri";
-import { MdOutlinePriceChange } from "react-icons/md";
-import { MdDriveFileRenameOutline } from "react-icons/md";
+import { MdOutlinePriceChange, MdDriveFileRenameOutline } from "react-icons/md";
+import LiveNftCard from "../../components/LiveNftCard/LiveNftCard";
+import nftImagePreview from "../../assets/IMAGE/SECTION/Astro.png";
 
 export default function CreateNft() {
   const {
@@ -20,15 +18,19 @@ export default function CreateNft() {
     (state) => state.nft
   );
 
+  const [previewImage, setPreviewImage] = useState(nftImagePreview);
+  const [nftName, setNftName] = useState("");
+  const [nftPrice, setNftPrice] = useState("");
+  const [nftDescription, setNftDescription] = useState("");
+
   const onSubmit = async (data) => {
     try {
       const formData = new FormData();
       formData.append("nftImage", data.nftImage[0]);
-
-      const { nftImage, ...remainingData } = data;
-
-      for (let key in remainingData) {
-        formData.append(key, data[key]);
+      for (let key in data) {
+        if (key !== "nftImage") {
+          formData.append(key, data[key]);
+        }
       }
       const resultAction = await dispatch(createNft(formData));
       console.log("Созданный NFT:", resultAction.payload);
@@ -37,82 +39,99 @@ export default function CreateNft() {
     }
   };
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setPreviewImage(URL.createObjectURL(file));
+    }
+  };
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900">
-      <div className="max-w-md w-full bg-gray-800 rounded-lg shadow-lg p-8">
-        <h1 className="text-3xl text-white font-bold mb-6">Create NFT</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-          <div>
-            <label className="flex items-center gap-2 text-sm font-medium text-gray-300 mb-2">
-              Name <MdDriveFileRenameOutline />
-            </label>
-            <input
-              type="text"
-              {...register("name", { required: true })}
-              className={`appearance-none rounded-xl relative block w-full px-3 py-2 border ${
-                errors.name ? "border-red-500" : "border-gray-700"
-              } bg-gray-700 text-white focus:outline-none focus:ring-purple-500 focus:border-purple-500`}
-            />
-            {errors.name && (
-              <p className="text-red-500 text-xs mt-1">Это поле обязательно.</p>
-            )}
-          </div>
-          <div>
-            <label className="flex items-center gap-2 text-sm font-medium text-gray-300 mb-2">
-              Price <MdOutlinePriceChange />
-            </label>
-            <input
-              type="number"
-              {...register("price", { required: true })}
-              className={`appearance-none rounded-xl relative block w-full px-3 py-2 border ${
-                errors.price ? "border-red-500" : "border-gray-700"
-              } bg-gray-700 text-white focus:outline-none focus:ring-purple-500 focus:border-purple-500`}
-            />
-            {errors.price && (
-              <p className="text-red-500 text-xs mt-1">Это поле обязательно.</p>
-            )}
-          </div>
-          <div>
-            <label className="flex items-center gap-2 text-sm font-medium text-gray-300 mb-2">
-              Image (URL) <RiNftFill className="pt-[2px]" />
-            </label>
-            <input
-              type="file"
-              {...register("nftImage", { required: true })}
-              className={`appearance-none rounded-xl relative block w-full px-3 py-2 border ${
-                errors.nftImage ? "border-red-500" : "border-gray-700"
-              } bg-gray-700 text-white focus:outline-none focus:ring-purple-500 focus:border-purple-500`}
-            />
-            {errors.nftImage && (
-              <p className="text-red-500 text-xs mt-1">Это поле обязательно.</p>
-            )}
-          </div>
-          <div>
-            <label className="flex items-center gap-2 text-sm font-medium text-gray-300 mb-2">
-              description <MdDriveFileRenameOutline />
-            </label>
-            <textarea
-              {...register("description", { required: true })}
-              className={`appearance-none rounded-xl relative block w-full px-3 py-2 border ${
-                errors.description ? "border-red-500" : "border-gray-700"
-              } bg-gray-700 text-white focus:outline-none focus:ring-purple-500 focus:border-purple-500`}
-            />
-            {errors.description && (
-              <p className="text-red-500 text-xs mt-1">Это поле обязательно.</p>
-            )}
-          </div>
-          <div>
-            <button
-              type="submit"
-              className={`w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white hover:text-black bg-purple-600 hover:bg-white  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 ${
-                nftLoading ? "opacity-50 cursor-not-allowed" : ""
-              }`}
-              disabled={nftLoading}
-            >
-              Create NFT
-            </button>
-          </div>
-        </form>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 p-4">
+      <div className="w-full max-w-4xl flex flex-col md:flex-row items-center justify-center gap-4 bg-gray-800 rounded-lg shadow-lg p-8">
+        <div className="w-full md:w-1/2">
+          <h1 className="text-3xl text-white font-bold mb-6">Создать NFT</h1>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+            <div>
+              <label className="flex items-center gap-2 text-sm font-medium text-gray-300 mb-2">
+                Название <MdDriveFileRenameOutline />
+              </label>
+              <input
+                type="text"
+                {...register("name", { required: true })}
+                onChange={(e) => setNftName(e.target.value)}
+                className={`appearance-none rounded-xl relative block w-full px-3 py-2 border ${
+                  errors.name ? "border-red-500" : "border-gray-700"
+                } bg-gray-700 text-white focus:outline-none focus:ring-purple-500 focus:border-purple-500`}
+              />
+              {errors.name && (
+                <p className="text-red-500 text-xs mt-1">
+                  Это поле обязательно.
+                </p>
+              )}
+            </div>
+            <div>
+              <label className="flex items-center gap-2 text-sm font-medium text-gray-300 mb-2">
+                Цена <MdOutlinePriceChange />
+              </label>
+              <input
+                type="number"
+                {...register("price", { required: true })}
+                onChange={(e) => setNftPrice(e.target.value)}
+                className={`appearance-none rounded-xl relative block w-full px-3 py-2 border ${
+                  errors.price ? "border-red-500" : "border-gray-700"
+                } bg-gray-700 text-white focus:outline-none focus:ring-purple-500 focus:border-purple-500`}
+              />
+              {errors.price && (
+                <p className="text-red-500 text-xs mt-1">
+                  Это поле обязательно.
+                </p>
+              )}
+            </div>
+            <div>
+              <label className="flex items-center gap-2 text-sm font-medium text-gray-300 mb-2">
+                Изображение (URL) <RiNftFill className="pt-[2px]" />
+              </label>
+              <input
+                type="file"
+                {...register("nftImage", { required: true })}
+                onChange={handleImageChange}
+                className={`appearance-none rounded-xl relative block w-full px-3 py-2 border ${
+                  errors.nftImage ? "border-red-500" : "border-gray-700"
+                } bg-gray-700 text-white focus:outline-none focus:ring-purple-500 focus:border-purple-500`}
+              />
+              {errors.nftImage && (
+                <p className="text-red-500 text-xs mt-1">
+                  Это поле обязательно.
+                </p>
+              )}
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className={`w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white hover:text-black bg-purple-600 hover:bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 ${
+                  nftLoading ? "opacity-50 cursor-not-allowed" : ""
+                }`}
+                disabled={nftLoading}
+              >
+                {nftLoading ? "Создание..." : "Создать NFT"}
+              </button>
+              {nftError && (
+                <p className="text-red-500 text-xs mt-2">Ошибка: {nftError}</p>
+              )}
+            </div>
+          </form>
+        </div>
+        <div className="w-full md:w-1/2 flex  p-4">
+          <LiveNftCard
+            imgUrl={previewImage}
+            title={nftName || "Название NFT"}
+            avatar={previewImage}
+            user={"Moon"}
+            price={nftPrice || "0"}
+          />
+        </div>
       </div>
     </div>
   );
