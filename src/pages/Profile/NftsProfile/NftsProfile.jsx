79cebf13334efd73b2c9d88779617e7d7a1a8bac
NABIL234234 <sarzanovnabil108@gmail.com>
2024-06-07 18:22:26 +0600

    feat: Map nfts

--- src/pages/Profile/NftsProfile/NftsProfile.jsx
+++ src/pages/Profile/NftsProfile/NftsProfile.jsx
@@ -1,126 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { fetchProfileInfo } from "../../../store/slices/nft";
 import CardMoreNft from "../../../components/CardMoreNft/CardMoreNft";
-// images
-
-import MoonDancer from "../../../../src/assets/IMAGE/PLAY.SVG/nav/MoonDancer.png";
-import NftsGrib from "../../../assets/IMAGE/SECTION/NftsGrib.png";
-import Nfts032 from "../../../assets/IMAGE/SECTION/Nfts032.png";
-import Nfts024 from "../../../assets/IMAGE/SECTION/Nfts024.png";
-import NftsBear from "../../../assets/IMAGE/SECTION/NftsBear.png";
-import NftsDog from "../../../assets/IMAGE/SECTION/NftsDog.png";
-import NftsRobot from "../../../assets/IMAGE/SECTION/NftsRobot.png";
-import NftsCherry from "../../../assets/IMAGE/SECTION/NftsCherry.png";
-import NftsSpaceTravel from "../../../assets/IMAGE/SECTION/NftsSpaceTravel.png";
-import NftsSunset from "../../../assets/IMAGE/SECTION/NftsSunset.png";
-import NftsDesert from "../../../assets/IMAGE/SECTION/NftsDesert.png";
-import NftsIceCream from "../../../assets/IMAGE/SECTION/NftsIceCream.png";
-import NftsDog2 from "../../../assets/IMAGE/SECTION/NftsDog2.png";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 export default function NftsProfile() {
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+  const { profile, loading, error } = useSelector((state) => state.nft);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchProfileInfo(id));
+    }
+  }, [id, dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!profile) {
+    return <div>No profile data available.</div>;
+  }
+
   return (
-    <>
-      <div className="pt-[60px] bg-zinc-700">
-        <div className="max-w-6xl mx-auto px-5 font-mono">
-          <div className="flex justify-start items-center flex-wrap ">
+    <div className="pt-[60px] bg-zinc-700">
+      <div className="max-w-6xl mx-auto px-5 font-mono">
+        <div className="flex justify-start items-center flex-wrap">
+          {profile.createdNfts.map((nft) => (
             <CardMoreNft
-              imgUrl={NftsGrib}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
+              key={nft.id}
+              imgUrl={nft.nftImage}
+              title={nft.name}
+              avatar={profile.profileImage}
+              user={profile.username}
+              price={`${nft.price}`}
             />
-            <CardMoreNft
-              imgUrl={Nfts032}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={Nfts024}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={NftsBear}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={NftsDog}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={NftsRobot}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={NftsCherry}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={NftsSpaceTravel}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={NftsSunset}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={NftsDesert}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={NftsIceCream}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-            <CardMoreNft
-              imgUrl={NftsDog2}
-              title="Distant Galaxy"
-              avatar={MoonDancer}
-              user="MoonDancer"
-              price="1.63 ETH"
-              Bid="0.33 wETH"
-            />
-          </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
