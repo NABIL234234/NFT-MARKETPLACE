2db1a049653ec47d7090ea6254eda0acc21a4595
NABIL234234 <sarzanovnabil108@gmail.com>
2024-06-07 10:41:30 +0600

    feat: Added temporarily MockData

--- src/pages/Home/MoreNtf/MoreNft.jsx
+++ src/pages/Home/MoreNtf/MoreNft.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import CardMoreNft from "../../../components/CardMoreNft/CardMoreNft";
 import See from "../../../components/buttons/See";
-
-// images
-
-import Galaxy from "../../../../src/assets/IMAGE/SECTION/Galaxy.png";
-import MoonDancer from "../../../../src/assets/IMAGE/PLAY.SVG/nav/MoonDancer.png";
-import Odena from "../../../../src/assets/IMAGE/SECTION/Odena.png";
-import Nebula from "../../../../src/assets/IMAGE/PLAY.SVG/nav/Nebula.png";
-import Astro from "../../../../src/assets/IMAGE/SECTION/Astro.png";
-import Spaceone from "../../../../src/assets/IMAGE/PLAY.SVG/nav/spaceone.png";
 import { useNavigate } from "react-router";
+import  mockNfts  from "../../../../server/MockData"; // Импортируем mock данные
 
 export default function MoreNft() {
-
   const navigate = useNavigate();
 
   const handleGetStarted = () => {
@@ -36,59 +27,33 @@ export default function MoreNft() {
                 <p className="text-xl pt-3">Explore new trending NFTs</p>
               </div>
               <div className="flex mb:hidden justify-start flex-wrap">
-                <CardMoreNft
-                  imgUrl={Galaxy}
-                  title="Distant Galaxy"
-                  avatar={MoonDancer}
-                  user="MoonDancer"
-                  price="1.63 ETH"
-                  Bid="0.33 wETH"
-                />
-                <CardMoreNft
-                  imgUrl={Odena}
-                  title="Life On Edena"
-                  avatar={Nebula}
-                  user="NebulaKid"
-                  price="1.63 ETH"
-                  Bid="0.33 wETH"
-                />
-                <CardMoreNft
-                  imgUrl={Astro}
-                  title="AstroFiction"
-                  avatar={Spaceone}
-                  user="Spaceone"
-                  price="1.63 ETH"
-                  Bid="0.33 wETH"
-                />
+                {mockNfts.map((nft) => (
+                  <CardMoreNft
+                    key={nft.id}
+                    id={nft.id}
+                    imgUrl={nft.nftImage}
+                    title={nft.name}
+                    avatar={nft.avatar}
+                    user={nft.creatorUsername}
+                    price={nft.price}
+                  />
+                ))}
               </div>
               <See onClick={handleGetStarted} />
             </div>
 
             <div className="hidden mb:flex justify-start flex-wrap">
-              <CardMoreNft
-                imgUrl={Galaxy}
-                title="Distant Galaxy"
-                avatar={MoonDancer}
-                user="MoonDancer"
-                price="1.63 ETH"
-                Bid="0.33 wETH"
-              />
-              <CardMoreNft
-                imgUrl={Odena}
-                title="Life On Edena"
-                avatar={Nebula}
-                user="NebulaKid"
-                price="1.63 ETH"
-                Bid="0.33 wETH"
-              />
-              <CardMoreNft
-                imgUrl={Astro}
-                title="AstroFiction"
-                avatar={Spaceone}
-                user="Spaceone"
-                price="1.63 ETH"
-                Bid="0.33 wETH"
-              />
+              {mockNfts.map((nft) => (
+                <CardMoreNft
+                  key={nft.id}
+                  id={nft.id}
+                  imgUrl={nft.nftImage}
+                  title={nft.name}
+                  avatar={nft.avatar}
+                  user={nft.creatorUsername}
+                  price={nft.price}
+                />
+              ))}
             </div>
           </div>
         </div>
