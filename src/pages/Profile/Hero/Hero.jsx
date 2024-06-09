1950496bf207484976ddb6ac45bc7dc40558150d
NABIL234234 <sarzanovnabil108@gmail.com>
2024-06-09 15:40:55 +0600

    style: fix tabs and translate value

--- src/pages/Profile/Hero/Hero.jsx
+++ src/pages/Profile/Hero/Hero.jsx
@@ -2,30 +2,26 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProfileInfo } from "../../../store/slices/nft";
 import "./Hero.css";
-import GetStart from "../../../components/buttons/GetStart";
-
-// Изображения
-import Anumakid from "../../../assets/IMAGE/SECTION/Anumakig_profile.png";
-import Copy from "../../../assets/IMAGE/PLAY.SVG/nav/Copy.svg";
+import { motion } from "framer-motion";
+import { CiInstagram } from "react-icons/ci";
+import { FaTelegramPlane } from "react-icons/fa";
 import { SlUserFollow } from "react-icons/sl";
-import Globe from "../../../assets/IMAGE/PLAY.SVG/nav/Globe.svg";
-import Discord from "../../../assets/IMAGE/SECTION/Discord.svg";
-import Youtube from "../../../assets/IMAGE/SECTION/Youtube.svg";
-import Twitter from "../../../assets/IMAGE/SECTION/Twitter.svg";
-import Instagram from "../../../assets/IMAGE/SECTION/instagram.svg";
 import { useParams } from "react-router";
+import Anumakid from "../../../assets/IMAGE/SECTION/Anumakig_profile.png";
+
+const TABS = [
+  { label: "Created", value: "createdNfts" },
+  { label: "Owned", value: "ownedByNfts" },
+  // { label: "Коллекция", value: "collection" },
+];
 
 export default function Hero() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const { profile, loading, error } = useSelector((state) => state.nft);
   const [selectedTab, setSelectedTab] = useState("createdNfts");
-  const [followersCount, setFollowersCount] = useState(
-    profile?.followersCount || 0
-  );
-  const [isFollowed, setIsFollowed] = useState(
-    localStorage.getItem("isFollowed") === "true"
-  );
+  const [followersCount, setFollowersCount] = useState(profile?.followersCount || 0);
+  const [isFollowed, setIsFollowed] = useState(localStorage.getItem("isFollowed") === "true");
 
   useEffect(() => {
     if (id) {
@@ -33,15 +29,14 @@ export default function Hero() {
     } else {
       console.error("ID должен быть числом");
     }
-    console.log("get date");
   }, [dispatch, id]);
 
   if (loading) {
-    return <div>Загрузка...</div>;
+    return <div>Loading..ю</div>;
   }
 
   if (!profile) {
-    return <div>Профиль не найден</div>;
+    return <div>Profile not found</div>;
   }
 
   const handleTabChange = (tab) => {
@@ -61,23 +56,15 @@ export default function Hero() {
     <>
       <div className="HeaderChannel" />
       <div className="max-w-6xl mx-auto px-5 font-mono">
-        <div className="absolute top-[370px] lgg:top-[393px] z-[1] ">
-          <img
-            src={profile?.profileImage || Anumakid}
-            alt={profile?.username || "Профиль"}
-          />
+        <div className="absolute top-[370px] lgg:top-[393px] z-[1]">
+          <img src={profile?.profileImage || Anumakid} alt={profile?.username || "Profile"} />
         </div>
         <div className="pt-[80px]">
           <div className="block mdd:flex items-center">
-            <h2 className="text-white text-6xl font-semibold">
-              {profile?.username || "Неизвестный пользователь"}
-            </h2>
+            <h2 className="text-white text-4xl font-semibold">{profile?.username || "Unknown user"}</h2>
             <div className="flex gap-[20px] ml-auto">
-              <div>
-                <GetStart imgUrl={Copy} desc={"0xc0e3b79c"} />
-              </div>
               <button
-                className="flex w-full mb:w-48 items-center justify-center gap-3 rounded-xl text-white border-2 border-purple-500 p-2 mt-6"
+                className="flex w-full mb:w-48 items-center justify-center gap-3 rounded-xl transition ease-in-out delay-15 text-white border-2 border-purple-500 hover:bg-purple-500 active:bg-purple-700 p-4 mt-6"
                 onClick={handleFollow}
               >
                 <SlUserFollow />
@@ -87,86 +74,45 @@ export default function Hero() {
           </div>
           <div className="flex gap-[8px] sm:gap-[40px] smm:gap-[15px] mt-[30px] text-xl smm:text-2xl text-white">
             <div>
-              <h2 className="font-semibold">{profile?.volume || 0}+</h2>
-              <h2 className="text-sm smm:text-xl">Объем</h2>
+              <h2 className="font-semibold">{profile?.volume}+</h2>
+              <h2 className="text-sm smm:text-xl">Volume</h2>
             </div>
             <div>
               <h2 className="font-semibold">+</h2>
-              <h2 className="text-sm smm:text-xl">Продано NFT</h2>
+              <h2 className="text-sm smm:text-xl">Sold NFT</h2>
             </div>
             <div>
               <h2 className="font-semibold">{followersCount}+</h2>
-              <h2 className="text-sm smm:text-xl">Подписчики</h2>
+              <h2 className="text-sm smm:text-xl">Subscribers</h2>
             </div>
           </div>
           <div className="pt-[30px]">
-            <h4 className="text-stone-400 text-lg">Ссылки</h4>
-            <div className="w-22% flex gap-[12px] pt-[5px]">
-              {profile?.website && (
-                <a href={profile?.website}>
-                  <img src={Globe} alt="Globe" />
-                </a>
-              )}
-              {profile.discord && (
-                <a href={profile.discord}>
-                  <img src={Discord} alt="Discord" />
-                </a>
-              )}
-
-              {profile.youtube && (
-                <a href={profile.youtube}>
-                  <img src={Youtube} alt="Youtube" />
-                </a>
-              )}
-              {profile.twitter && (
-                <a href={profile.twitter}>
-                  <img src={Twitter} alt="Twitter" />
-                </a>
-              )}
-              {profile.instagram && (
-                <a href={profile.instagram}>
-                  <img src={Instagram} alt="Instagram" />
-                </a>
-              )}
+            <h4 className="text-stone-400 text-lg">Links</h4>
+            <div className="w-72% flex gap-[12px] pt-[5px] text-purple-500">
+              <a href="https://www.instagram.com/magic_nftmarcketplace?igsh=ZmplY3c0ZTI4eWI5" className="text-3xl"><CiInstagram /></a>
+              <a href="https://t.me/magic_nft_marketplace" className="text-3xl"><FaTelegramPlane /></a>
             </div>
           </div>
         </div>
-        <div className="flex mt-[80px]">
-          <div className="flex justify-center items-center gap-[16px] w-[525px] pb-[14.5px] relative">
-            <h3
-              className={`text-white font-semibold cursor-pointer ${
-                selectedTab === "createdNfts"
-                  ? "border-b-4 border-purple-600 transition-all duration-100 animate-slideInRight"
-                  : ""
-              }`}
-              onClick={() => handleTabChange("createdNfts")}
-            >
-              Создано
-            </h3>
-            <div>
-              <h5 className="text-white"></h5>
-            </div>
-          </div>
-          <div className="flex justify-center items-center gap-[16px] w-[525px] pb-[14.5px] relative">
-            <h3
-              className={`text-white font-semibold cursor-pointer ${
-                selectedTab === "ownedByNfts"
-                  ? "border-b-4 border-purple-600 animate-slideInRight"
-                  : ""
-              }`}
-              onClick={() => handleTabChange("ownedByNfts")}
-            >
-              Принадлежит
-            </h3>
-            <div>
-              <h5 className="text-white"></h5>
-            </div>
-          </div>
-          <div className="flex justify-center items-center gap-[16px] w-[525px] pb-[14.5px]">
-            <h3 className="text-white font-semibold">Коллекция</h3>
-            <div className="hidden mb:flex bg-zinc-400 w-[37px] pt-0.5 pb-0.5 px-2.5 rounded-full">
-              <h5 className="text-white">{profile?.collection?.length || 0}</h5>
-            </div>
+        <div className="flex flex-col items-center mt-[80px] relative">
+          <div className="flex gap-[116px] pb-2">
+            {TABS.map((tab) => (
+              <div key={tab.value} className="relative">
+                <h3
+                  className={`text-white  cursor-pointer text-xl ${selectedTab === tab.value ? "text-purple-600" : ""}`}
+                  onClick={() => handleTabChange(tab.value)}
+                >
+                  {tab.label}
+                </h3>
+                {selectedTab === tab.value && (
+                  <motion.div
+                    layoutId="underline"
+                    className="absolute bottom-0 h-0.5 bg-purple-600 w-full"
+                    transition={{ type: "spring", stiffness: 500, damping: 30 }}
+                  />
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
