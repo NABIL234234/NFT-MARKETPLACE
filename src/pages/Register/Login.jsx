a4f912001230bd76b4868525c519a4e190352d96
NABIL234234 <sarzanovnabil108@gmail.com>
2024-06-12 02:19:55 +0600

    style: Add window error

--- src/pages/Register/Login.jsx
+++ src/pages/Register/Login.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { useNavigate, useLocation, NavLink } from "react-router-dom";
 import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode"; // Используйте правильный импорт
+import {jwtDecode} from "jwt-decode"; // Fixed import
 import { getUserLogin } from "../../store/actions/asyncAction";
 import Inputs from "../../components/inputs/Inputs";
 import { FaKey, FaUser, FaUnlockAlt } from "react-icons/fa";
@@ -14,33 +14,28 @@ import SingUpImg from "../../assets/IMAGE/SECTION/SingUpImg.png";
 import User from "../../assets/IMAGE/PLAY.SVG/nav/User.png";
 import Password from "../../assets/IMAGE/PLAY.SVG/nav/LockKey.svg";
 
-export default function Login() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+import "./login.scss"
 
+export default function Login() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const [showPassword, setShowPassword] = useState(false);
+  const [loginError, setLoginError] = useState("");
   const dispatch = useDispatch();
   const location = useLocation();
   const from = location.state?.from || "/";
-  const navigate = useNavigate(); // Используем useNavigate для перехода на страницу профиля
+  const navigate = useNavigate();
 
   const onSubmit = async (data) => {
     try {
-      const resultAction = await dispatch(
-        getUserLogin({ newUser: data, navigate })
-      );
-      if (getUserLogin.fulfilled.match(resultAction)) {
-        console.log("Данные с сервера:", resultAction.payload);
+      const resultAction = await dispatch(getUserLogin({ newUser: data, navigate }));
+      if (resultAction.meta.requestStatus === "fulfilled") {
         const { id, tokens } = resultAction.payload;
-        console.log("Полученный токен:", tokens.access_token);
         localStorage.setItem("accessToken", tokens.access_token);
         localStorage.setItem("userId", id);
         window.dispatchEvent(new Event("storage"));
         navigate(from, { replace: true });
       } else {
+        setLoginError("Неправильное имя пользователя или пароль");
         console.error("Ошибка при выполнении запроса:", resultAction.payload);
       }
     } catch (error) {
@@ -50,26 +45,28 @@ export default function Login() {
 
   const handleGoogleSuccess = (credentialResponse) => {
     const decoded = jwtDecode(credentialResponse.credential);
-    console.log("Decoded Google Token:", decoded);
     localStorage.setItem("accessToken", credentialResponse.credential);
-    localStorage.setItem("userId", decoded.sub); // Используем sub как userId
+    localStorage.setItem("userId", decoded.sub);
     window.dispatchEvent(new Event("storage"));
-    navigate(`/profile/${decoded.sub}`); // Переходим на страницу профиля с использованием sub в качестве ID пользователя
+    navigate(`/profile/${decoded.sub}`);
   };
 
   const handleGoogleError = () => {
     console.log("Login Failed");
   };
 
+  const handleCloseError = () => {
+    setLoginError("");
+  };
+
+
+  
   return (
     <section className="mdd:flex gap-[15px] md:gap-[40px] lg:gap-[60px] xl:gap-[150px] font-mono">
       <div>
         <img src={SingUpImg} alt="SingUpImg" />
       </div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex justify-center items-center p-[15px]"
-      >
+      <form onSubmit={handleSubmit(onSubmit)} className="flex justify-center items-center p-[15px]">
         <div className="flex flex-col">
           <div className="text-white pt-[20px]">
             <h3 className="text-3xl md:text-4xl lg:text-5xl font-semibold">
@@ -87,14 +84,10 @@ export default function Login() {
                 type="text"
                 icons={User}
                 placeholder="Username"
-                {...register("username", {
-                  required: "Введите имя пользователя",
-                })}
+                {...register("username", { required: "Введите имя пользователя" })}
               />
               {errors.username && (
-                <span className="error absolute text-red-500 font-sans">
-                  {errors.username.message}
-                </span>
+                <span className="error absolute text-red-500 font-sans">{errors.username.message}</span>
               )}
             </div>
             <div className="relative">
@@ -105,60 +98,47 @@ export default function Login() {
                 placeholder="Password"
                 {...register("password", {
                   required: "Введите пароль",
-                  minLength: {
-                    value: 6,
-                    message: "Пароль должен содержать как минимум 6 символов!",
-                  },
+                  minLength: { value: 6, message: "Пароль должен содержать как минимум 6 символов!" },
                 })}
               />
               <div className="absolute top-[-6%] left-[84%] mb:left-[55%]">
-                <IconButton
-                  onClick={() => setShowPassword(!showPassword)}
-                  sx={{
-                    color: "black",
-                    position: "absolute",
-                    right: 10,
-                    top: 10,
-                  }}
-                  aria-label={showPassword ? "Hide password" : "Show password"}
-                >
+                <IconButton onClick={() => setShowPassword(!showPassword)} sx={{ color: "black", position: "absolute", right: 10, top: 10 }} aria-label={showPassword ? "Hide password" : "Show password"}>
                   {showPassword ? <VisibilityOff /> : <Visibility />}
                 </IconButton>
               </div>
               {errors.password && (
-                <span className="error absolute text-red-500 font-sans">
-                  {errors.password.message}
-                </span>
+                <span className="error absolute text-red-500 font-sans">{errors.password.message}</span>
               )}
             </div>
-            <button
-              type="submit"
-              className="flex justify-center items-center gap-[10px] w-[150px] h-[40px] rounded-xl bg-purple-500 text-white"
-            >
+            <button type="submit" className="flex justify-center items-center gap-[10px] w-[150px] h-[40px] rounded-xl text-white hover:text-black bg-purple-500 transition ease-in-out delay-15  hover:bg-white  active:bg-purple-400">
               <IoLogIn />
               Login
             </button>
             <div>
-              <NavLink
-                to="/register"
-                className="flex justify-center items-center gap-[20px] w-[300px] p-[3px] rounded-md bg-purple-500 text-white"
-              >
+              <NavLink to="/register" className="flex justify-center items-center gap-[20px] w-[300px] p-[3px] rounded-md text-white hover:text-black bg-purple-500 transition ease-in-out delay-15  hover:bg-white  active:bg-purple-400">
                 Are you registered?
               </NavLink>
             </div>
-            <div className="flex justify-center items-center gap-[20px] w-[300px] p-[3px] rounded-md bg-white">
+            <div className="flex justify-center items-center gap-[20px] w-[300px] p-[3px] rounded-md text-black hover:text-white bg-white transition ease-in-out delay-15  hover:bg-purple-500   active:bg-purple-400">
               <FaKey />
               <NavLink to="/confirmAccount">
                 Don't remember your password?
               </NavLink>
             </div>
-            <GoogleLogin
-              onSuccess={handleGoogleSuccess}
-              onError={handleGoogleError}
-            />
+            <GoogleLogin onSuccess={handleGoogleSuccess} onError={handleGoogleError} />
           </div>
         </div>
       </form>
+      {loginError && (
+        <div className="error-message-container">
+          <div className="error-message">
+            {loginError}
+            <button className="close-button" onClick={handleCloseError}>
+              &times;
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
