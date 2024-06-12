6a8b3c760725f0a141de933f63e66c9e8776c991
NABIL234234 <sarzanovnabil108@gmail.com>
2024-06-12 23:52:19 +0600

    style: Add motion.div for components

--- src/components/Card-Collection/CardCollection.jsx
+++ src/components/Card-Collection/CardCollection.jsx
@@ -1,24 +1,60 @@
-import React from "react";
-import './CardCollection.css'
+import React, { useState } from "react";
+import "./CardCollection.css";
+import { motion } from "framer-motion";
 
-export default function CardCollection({imgUrl, imgUrl2, imgUrl3, CollectionName, avatarUrl, nickName}) {
+export default function CardCollection({
+  imgUrl,
+  imgUrl2,
+  imgUrl3,
+  CollectionName,
+  avatarUrl,
+  nickName,
+}) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 2000); 
+  };
+
   return (
     <>
       <div className="CardCollection pt-4 sm:pt-16">
-        <div>
-          <div> 
+        <div onClick={handleClick}>
+          <motion.div whileHover={{ scale: 1.1 }}>
             <img src={imgUrl} alt="DOG" />
-          </div>
-          <div className="flex pt-4 gap-4">
-            <div className="">
-              <img src={imgUrl2} alt="CAT" />
-            </div>
-            <div>
-              <img src={imgUrl3} alt="BEAR" />
-            </div>
-            <div className="flex justify-center items-center p-6 bg-purple-500 rounded-2xl">
-              <h3 className="pl-1.5 text-white  font-semibold">1025+</h3>
-            </div>
+            {isClicked && <div className="development-icon animate"></div>}
+          </motion.div>
+          <div className="flex pt-4 gap-4  ml-[12px] rdd:ml-0 ">
+            <motion.div whileHover={{ scale: 1.1 }}>
+              <img src={imgUrl2} alt="CAT"/>
+              {isClicked && (
+                <div className="development-icon animate">
+                  В процессе разработки
+                </div>
+              )}
+            </motion.div>
+            <motion.div whileHover={{ scale: 1.1 }}>
+              <img src={imgUrl3} alt="BEAR" className="bear" />
+              {isClicked && (
+                <div className="development-icon animate">
+                  В процессе разработки
+                </div>
+              )}
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              className="hidden mdd:flex justify-center items-center p-6 bg-purple-500 rounded-2xl "
+            >
+              <h3 className="pl-1.5 text-white font-semibold">1025+</h3>
+              {isClicked && (
+                <div className="development-icon animate">
+                  В процессе разработки
+                </div>
+              )}
+            </motion.div>
           </div>
           <div className="text-white pt-3">
             <h4 className="text-lg">{CollectionName}</h4>
