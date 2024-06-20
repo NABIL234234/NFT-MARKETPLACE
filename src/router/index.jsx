b77c7c04dcbe85f0808c1720c43de8fec39ae175
NABIL234234 <sarzanovnabil108@gmail.com>
2024-06-21 01:40:18 +0600

    style: add animation for loading

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -8,33 +8,31 @@ import Rankings from "../pages/Rankings";
 import Wallet from "../pages/Wallet";
 import Info from "../pages/Info";
 import Profile from "../pages/Profile";
-
 import Register from "../pages/Register/CreateAccount";
 import Login from "../pages/Register/Login";
 import ConfirmAccount from "../pages/Register/ConfirmAccount";
 import Identification from "../pages/Register/Identification";
 import NewPassword from "../pages/Register/NewPassword";
 import SuccessChange from "../pages/Register/SuccessChange";
-
 import CreateNft from "../pages/CreateNft/CreateNft";
-
 import PrivateRoute from "../components/Private/PrivateRoute";
-
 import CreatorsProfile from "../pages/Profile/CreatorsProfile";
+import "./Spinner.scss"; // Assuming this is your spinner CSS
 
 export default function RouterView() {
   const location = useLocation();
   const [showLoading, setShowLoading] = useState(false);
 
-  
   useEffect(() => {
-    const pagesWithLoading = ["/shop"]; 
+    const pagesWithLoading = ["/shop"];
     const shouldShowLoading = pagesWithLoading.includes(location.pathname);
     if (shouldShowLoading) {
       setShowLoading(true);
       setTimeout(() => {
         setShowLoading(false);
-      }, 1000); 
+      }, 1000);
+    } else {
+      setShowLoading(false);
     }
   }, [location]);
 
@@ -45,11 +43,8 @@ export default function RouterView() {
     { path: "/wallet", element: <PrivateRoute><Wallet /></PrivateRoute> },
     { path: "/info/:id", element: <PrivateRoute><Info /></PrivateRoute> },
     { path: "/profile/:id", element: <PrivateRoute><Profile /></PrivateRoute> },
-  
     { path: "/CreatorsProfile/:id", element: <PrivateRoute><CreatorsProfile /></PrivateRoute> },
-  
     { path: "/createNft", element: <PrivateRoute><CreateNft /></PrivateRoute> },
-
     { path: "/register", element: <Register /> },
     { path: "/login", element: <Login /> },
     { path: "/confirmAccount", element: <ConfirmAccount /> },
@@ -59,7 +54,7 @@ export default function RouterView() {
   ]);
 
   return (
-    <AnimatePresence exitBeforeEnter={false}>
+    <AnimatePresence mode="wait">
       {showLoading && (
         <motion.div
           key="loading"
@@ -67,19 +62,18 @@ export default function RouterView() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }} // Smooth transition
         >
-  
-          <div className="text-center text-5xl text-White">
-            Loading...
-          </div>
+          <div className="spinner"></div>
         </motion.div>
       )}
       {!showLoading && (
         <motion.div
           key={location.pathname}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
+          initial={{ opacity: 0, y: -50 }} // Initial position above the screen
+          animate={{ opacity: 1, y: 0 }} // Final position at the center of the screen
+          exit={{ opacity: 0, y: 50 }} // Exit position below the screen
+          transition={{ duration: 0.5 }} // Smooth transition
         >
           {element}
         </motion.div>
